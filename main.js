79214ed198fc87f4d28ee79a556cc1f3b43e3404
Panupong Pasupat <ppasupat@stanford.edu>
2019-02-01 21:40:47 -0800

    Init

---
+++ main.js
@@ -0,0 +1,43 @@
+$(function () {
+
+  const DEBUG = true;
+
+  // ################################
+  // 
+
+  function showMainScene() {
+    $('#scene-main').show();
+    $('#scene-preload').hide();
+
+  }
+
+  // ################################
+  // READY!!
+
+  function preload() {
+    // TODO
+    $('#pane-loading')
+      .empty()
+      .append($('<button>').text('เริ่มเล่น!').click(showMainScene));
+  }
+
+  // Handle screen resizing to 800 x 500
+  // https://stackoverflow.com/q/8735457
+  function resizeScreen() {
+    var sW = Math.min(window.screen.width, $(window).width());
+    var sH = Math.min(window.screen.height, $(window).height()) - 25;
+    var ratio = Math.min(sW / 800, sH / 500);
+    $('#viewport').attr('content',
+      'width='
+      + (ratio >= 1 ? 'device-width' : (ratio * 800))
+      + ', initial-scale='
+      + (ratio >= 1 ? 1.0 : ratio)
+      + ', user-scalable=no');
+    window.scroll(0, 1);
+  }
+
+  resizeScreen();
+  $(window).resize(resizeScreen);
+  preload();
+
+});
